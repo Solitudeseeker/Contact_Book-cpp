51953c5baf5cd74f888871b59668791b80015e9d
Solitudeseeker <firozjimachhar7@gmail.com>
2026-09-20 15:59:47 +0530

    Add user input for contact

--- main.cpp
+++ main.cpp
@@ -11,9 +11,13 @@ class Contact{
 };
 int main(){
     Contact c1;
-    c1.name="Adi";
-    c1.phone="6370409493";
-    c1.email="firozjimachhar7@gmail.com";
+    cout<<"Enter name: ";
+    getline(cin, c1.name);
+    cout<<"Enter phone: ";
+    getline(cin, c1.phone);
+    cout<<"Enter email: ";
+    getline(cin, c1.email);
+    cout<<"\n -----Contact Details-----\n";
     cout<<"Name: "<<c1.name<<endl;
     cout<<"Phone: "<<c1.phone<<endl;
     cout<<"Email: "<<c1.email<<endl;
