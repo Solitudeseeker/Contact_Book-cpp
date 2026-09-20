98aca1c702abbdde3c7c8f7d54d09b877fba93c0
Solitudeseeker <firozjimachhar7@gmail.com>
2026-09-20 16:43:16 +0530

    Add multiple contact support

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<cctype>
 
 using namespace std;
 
@@ -18,21 +17,37 @@ class Contact{
     }
 };
 int main(){
+    char another;
     vector <Contact> contacts;
     string name, phone, email;
-    cout<<"Enter name: ";
-    getline(cin, name);
-    cout<<"Enter phone: ";
-    getline(cin, phone);
-    cout<<"Enter email: ";
-    getline(cin, email);
-    Contact c1(name, phone, email);
-    contacts.push_back(c1);
+    do{
+        cout<<"Enter name: ";
+        getline(cin, name);
+        cout<<"Enter phone: ";
+        getline(cin, phone);
+        cout<<"Enter email: ";
+        getline(cin, email);
+        Contact c1(name, phone, email);
+        contacts.push_back(c1);
+        cout<<"Do you want to add another contact? (Y/N): ";
+        cin>>another;
+        while(another != 'Y' && another != 'y' && another != 'N' && another != 'n'){
+            cout<<"Invalid input. Please enter Y or N."<<endl;
+            cin>>another;
+        }
+        cin.ignore();
+        if(another== 'N' || another == 'n'){
+            cout<<"Thankyou for using Contact Book!"<<endl;
+        }else{
+            cout<<"Adding another contact..."<<endl;
+        }
+    }while(another == 'Y' || another == 'y');
     cout<<"\n -----Contact Details-----\n";
     for(const auto& contact : contacts){
         cout<<"Name: "<<contact.name<<endl;
         cout<<"Phone: "<<contact.phone<<endl;
         cout<<"Email: "<<contact.email<<endl;
+        cout<<"--------------------------\n";
     }
     return 0;
 }
